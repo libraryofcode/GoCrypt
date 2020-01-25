45a90a490d16d4f17ba7a93a7ee13094ac41871b
Matthew R <matthew@staff.libraryofcode.org>
2020-01-25 18:04:58 -0500

    RandomInt utility function

--- lib/classes/Util.ts
+++ lib/classes/Util.ts
@@ -1,6 +1,11 @@
-// eslint-disable-next-line max-classes-per-file
 import ffi from 'ffi-napi';
 
+/**
+ * **INTERNAL** Constructor is not meant to be called externally.
+ * *Includes Shared Object/Library Functions [GO]*
+ * @class
+ * @classdesc A general Utilities library.
+ */
 export default class Util {
   protected goLib: {
     RandomInt(max: number): { readCString(): string; },
@@ -14,7 +19,16 @@ export default class Util {
     });
   }
 
+  /**
+   * This function produces a random integer value
+   * [Shared Object Function (Go)]
+   * @param max Value provided should be >= -2147483648 and <= 2147483647
+   * @example Util.randomInt(500);
+   */
   public randomInt(max: number) {
+    if (typeof max !== 'number') {
+      throw new TypeError(`Expected type 'number' for parameter value, received type of '${typeof max}'`);
+    }
     const goResponse = this.goLib.RandomInt(max);
     const str = goResponse.readCString();
     const response: { Num: number, Err: string } = JSON.parse(str);
