cafd8d668aa19ec21e56546b48d058f9c032226c
Matthew R <matthew@staff.libraryofcode.org>
2020-02-29 17:06:01 -0500

    feat(lib/go/exe/util.go): Add random integer generation for Windows & Mac w/o shared libraries

--- lib/go/exe/main.go
+++ lib/go/exe/main.go
@@ -25,13 +25,15 @@ func HandleErrorFMT(err error, message string) {
 }
 
 func main() {
-	if len(os.Args) < 1 {
+	if len(os.Args) <= 1 {
 		HandleErrorFMT(errors.New("invalid command or command not found"), "[GO] Invalid command or command not found.")
 	}
 	if os.Args[1] == "pkinfo" {
 		PrivateKeyInfo()
 	} else if os.Args[1] == "csr" {
 		CSR()
+	} else if os.Args[1] == "randint" {
+		RandomInt()
 	} else {
 		HandleErrorFMT(errors.New("invalid command or command not found"), "[GO] Invalid command or command not found.")
 	}
---
+++ lib/go/exe/util.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"crypto/rand"
+	"math/big"
+	"strconv"
+	"encoding/json"
+	"errors"
+)
+
+// RandomInt Command handler for generating a random integer.
+func RandomInt() {
+	if len(os.Args) < 3 {
+		HandleErrorFMT(errors.New("expected arguments"), "[GO] Internal Error: Didn't receive enough arguments.")
+	}
+	max, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		HandleErrorFMT(err, "[GO] Could not parse input integer.")
+	}
+	random, err := rand.Int(rand.Reader, big.NewInt(int64(max)))
+	if err != nil {
+		HandleErrorFMT(err, "[GO] Could generate integer.")
+	}
+	output := struct{
+		Message int64
+	}{
+		Message: random.Int64(),
+	}
+	out, err := json.Marshal(&output)
+	if err != nil {
+		HandleErrorFMT(err, "[GO] Failed to marshal output data.")
+	}
+	fmt.Println(string(out))
+}
