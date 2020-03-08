be5c2617693fc3350c963a149f5c525f14e5bd4b
Matthew R <matthew@staff.libraryofcode.org>
2020-03-07 20:41:43 -0500

    fix(lib/index.ts): Remove redudant functions

--- lib/index.ts
+++ lib/index.ts
@@ -1,45 +1,4 @@
-/* eslint-disable no-console */
-import os from 'os';
-import { execSync } from 'child_process';
 import * as classes from './classes';
-
-/**
- * **INTERNAL** This function is internal and not callable by external callers.
- * Checks system information and determines if the user can run GoCrypto.
- * Go must be installed on the machine, and said machine has to be Linux. GoCrypto is not supported
- * on any other operating system but Linux.
- * @private
- * @internal
- */
-function checkRequirements(): { osCheck: boolean, goCheck: boolean } {
-  const res = { osCheck: true, goCheck: true };
-  if (os.platform() !== 'linux') {
-    res.osCheck = false;
-  }
-  const goStatus = () => {
-    try {
-      execSync('go version');
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
-  const goCheck = goStatus();
-  if (!goCheck) {
-    res.goCheck = false;
-  }
-  return res;
-}
-
-/**
- * Runs checks, throws errors if any of the checks return false.
- * If any check returns false, the thrown error will prevent the module from being required/imported.
- * @private
- * @internal
- */
-const check = checkRequirements();
-if (!check.osCheck) throw new Error('GoCrypt is only supported on Linux operating systems at this time.');
-if (!check.goCheck) throw new Error('GoCrypt requires the Go programming language to be installed and available in path.');
 
 export default {
   keys: new classes.Keys(),
