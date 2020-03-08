966c8be489cabce4470fe61ff2c7b7d5808799ab
Matthew R <matthew@staff.libraryofcode.org>
2020-03-07 20:41:27 -0500

    feat(lib/structs/Certificate.ts): certificate struct

--- lib/structs/index.ts
+++ lib/structs/index.ts
@@ -1,4 +1,4 @@
-export { default as Certificate } from './Certificate';
+export { default as Certificate, CertificateData } from './Certificate';
 export { default as Collection } from './Collection';
 export { default as CSR } from './CSR';
 export { default as PrivateKey } from './PrivateKey';
