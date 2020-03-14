ec7fe5520b9a3c823c5cd5a6eb2bceace9f93edf
Matthew R <matthew@staff.libraryofcode.org>
2020-03-13 20:28:02 -0400

    fix(*): Rename classes dir

--- lib/classes/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Certificates, CertificateRequestData } from './Certificates';
-export { default as Hash } from './Hash';
-export { default as Keys } from './Keys';
-export { default as Util } from './Util';
